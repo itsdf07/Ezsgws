2847e8255a0142199ea741cd6242bb73334f1761
itsdf07 <923255742@qq.com>
2015-07-05 18:28:49 +0800

    为角色实体类提供外部使用方法

--- src/com/sdf/ezsgws/dto/FigureDto.java
+++ src/com/sdf/ezsgws/dto/FigureDto.java
@@ -1,5 +1,6 @@
 package com.sdf.ezsgws.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +14,7 @@ public class FigureDto {
 
 	private String id;
 
+	/** 人物名称 */
 	private String name;
 	/** 人物头像 */
 	private String headIcon;
@@ -21,6 +23,68 @@ public class FigureDto {
 	/** 专属 */
 	private String only;
 	/** 技能 */
-	private List<SkillsDto> skills;
+	private List<SkillsDto> skillsList = new ArrayList<SkillsDto>();
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHeadIcon() {
+		return headIcon;
+	}
+
+	public void setHeadIcon(String headIcon) {
+		this.headIcon = headIcon;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getOnly() {
+		return only;
+	}
+
+	public void setOnly(String only) {
+		this.only = only;
+	}
+
+	public List<SkillsDto> getSkillsList() {
+		return skillsList;
+	}
+
+	public void setSkillsList(List<SkillsDto> skillsList) {
+		this.skillsList = skillsList;
+	}
+
+	/**
+	 * 添加技能信息到技能列表中
+	 *
+	 * @param skill
+	 *            技能
+	 */
+	public void addSkillToList(SkillsDto skill) {
+		if (skillsList == null) {
+			skillsList = new ArrayList<SkillsDto>();
+		}
+		skillsList.add(skill);
+
+	}
 
 }
--- src/com/sdf/ezsgws/dto/SkillsDto.java
+++ src/com/sdf/ezsgws/dto/SkillsDto.java
@@ -19,4 +19,44 @@ public class SkillsDto {
 	/** 技能信息 */
 	private String info;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getShortCut() {
+		return shortCut;
+	}
+
+	public void setShortCut(String shortCut) {
+		this.shortCut = shortCut;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
 }
